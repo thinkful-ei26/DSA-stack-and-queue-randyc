11122048ed81271d71d2a160c9c429f2448de0b3
R-Cole <randy@goodglue.com>
2019-01-18 13:05:08 -0700

    push, pop with find, peek and display all working

--- StackTwo.js
+++ StackTwo.js
@@ -5,8 +5,8 @@ class _Node{
 
   constructor(data,next) {
 
-    this.data;
-    this.next;
+    this.data = data;
+    this.next = next;
 
   }
  
@@ -24,8 +24,7 @@ class Stack {
   push(data) {
 
     console.log('PUSH');
-    console.log('data in ',data);
- 
+     
     if(this.top === null){
 
       this.top = new _Node(data,null);
@@ -38,14 +37,71 @@ class Stack {
  
   }
 
-  
+  //pop with find option
+  pop(find){
+
+    console.log('POP');
+
+    //do not use the find option
+    if(!find){
+
+      const topValue = this.top.data;
+      this.top = this.top.next;
+      return topValue;
+ 
+    }
+
+    //use the find option
+    let currentNode = this.top;
+    let prevNode = currentNode.next;
+
+    while(find !== currentNode.data){
+
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+ 
+    }
+
+
+    prevNode.next = currentNode.next;
+    return currentNode;
+    
+ 
+  }
+
+  peek() {
+
+    return this.top;
+
+  } 
+
+  display() {
+
+    console.log('----------------------------------');
+    console.log('This is the contents of the list: ');
+
+    let currentNode = this.top;
+
+    while(currentNode !== null){
+ 
+      console.log(currentNode);
+
+      currentNode = currentNode.next;
+
+    }
+
+    console.log('----------------------------------');
+ 
+    return;
+
+  }
 
 
 }
 
-let peek = (storedData) => {
+let outsidePeek = (storedData) => {
 
-  console.log('Current top value is: ',storedData.top);
+  console.log('Current top value is: ',storedData.top.data);
 
   return;
 
@@ -58,8 +114,20 @@ function Main() {
   const starTrek = new Stack();
 
   starTrek.push('Kirk');
-  
-  console.log(peek(starTrek));
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+  console.log('this is the top node: ',starTrek.peek());
+
+  starTrek.display();
+
+
+  // starTrek.pop();
+  // console.log('this is the top: ',starTrek.peek());
+
+  starTrek.pop('McCoy');
+   
+  starTrek.display();
    
 }
 
